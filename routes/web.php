75c8c3c4fe97c1abf52a2ab016b1f2a4f1c47c2c
Swienn <sven.tesselaar@student.uva.nl>
2025-06-04 21:10:05 +0200

    Voeg testroute toe met User factory

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+ * Test call of the first API.
+ * 
+ */
+Route::get('/test', function() {
+    return response()->json(User::factory()->make()->toArray());
 });
