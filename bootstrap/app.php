eabbaf521bda538d8cc9b4d582e65299acdf3aa3
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-10 11:30:00 +0200

    Require JSON and show JSON headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,30 @@
 <?php
 
+use App\Http\Middleware\RequireJsonMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(prepend: [
+            RequireJsonMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->shouldRenderJsonWhen(function (Request $request) {
+            /* Render JSON for API requests */
+            if ($request->is('api/*')) {
+                return true;
+            }
+
+            /* Render JSON for requests that expect JSON */
+            return $request->expectsJson();
+        });
     })->create();
