2a919268b3d5aa2c987aee502dd837508adb6ea8
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-10 13:51:30 +0200

    fix

--- database/migrations/2025_06_04_000006_create_submissions_table.php
+++ database/migrations/2025_06_04_000006_create_submissions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('submissions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('question_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
-            $table->foreignId('student_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
 
             $table->text('answer');
             $table->text('feedback')->nullable();
