91c603577ef22f2b717f3475063e2418d0e65876
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-10 14:41:11 +0200

    Installed intertia

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/', fn() => Inertia::render('welcome'))->name('home');
+
+Route::get('/login', LoginController::class)->name('login');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::get('dashboard', fn() => Inertia::render('dashboard'))->name('dashboard');
 });
