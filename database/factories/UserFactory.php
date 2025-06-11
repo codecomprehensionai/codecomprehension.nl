cb5867b5c4f46b3464072e44fbedb66cee89eec7
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-11 14:57:06 +0200

    Extend user model with lti columns

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password'          => static::$password ??= Hash::make('password'),
             'remember_token'    => Str::random(10),
+            'avatar_url'        => fake()->imageUrl(640, 480, 'people', true),
+            'locale'            => 'en-GB',
+            'lti_user_id'       => fake()->unique()->uuid(),
         ];
     }
 
@@ -38,21 +41,21 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
 
     public function student(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'type' => UserType::Student,
         ]);
     }
 
     public function teacher(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'type' => UserType::Teacher,
         ]);
     }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('avatar_url')->nullable();
+            $table->string('locale')->default('en-GB');
+            $table->string('lti_user_id')->unique();
             $table->timestamps();
         });
 
