d8b0b2640eeef7f68d64cac052966c043e8edf5c
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-16 13:29:19 +0200

    wip

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->as('v1:')->group(function (): void {
-    Route::webhooks('canvas/assignment', 'canvas.assignment.create');
-    Route::webhooks('canvas/assignment/update', 'canvas.assignment.update');
+    //
 });
