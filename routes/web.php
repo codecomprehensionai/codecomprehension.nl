43dbab2f4884d07e58e455c997512e402614f9bf
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-16 15:57:38 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@ Route::get('/', fn () => Inertia::render('welcome'))->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', fn () => Inertia::render('dashboard'))->name('dashboard');
 
-    // Teacher dashboard
     Route::get('teacher', [TeacherDashboardController::class, 'index'])->name('teacher.dashboard');
-
-    // Student dashboard
     Route::get('student', [StudentDashboardController::class, 'index'])->name('student.dashboard');
 });
