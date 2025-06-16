04dcd4a33f17df150f91896d1b1e4b5cace25855
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-16 17:47:08 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,23 @@
 
 use App\Http\Controllers\StudentDashboardController;
 use App\Http\Controllers\TeacherDashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+Route::get('loginError', fn() => 'todo')
+    ->middleware('guest')
+    ->name('login');
+
+Route::get('login', function () {
+    Auth::loginUsingId(1, true);
+
+    return redirect('user');
+});
+
+Route::get('user', function () {
+    return Auth::user() ?? 'unauthenticated';
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', fn() => Inertia::render('dashboard'))->name('dashboard');
