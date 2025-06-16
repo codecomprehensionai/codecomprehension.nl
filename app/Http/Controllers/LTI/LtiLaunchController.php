c5c60e2620c3eddbc98d0875659c920a42f9d6f3
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-16 15:05:58 +0200

    wip

--- app/Http/Controllers/LTI/LtiLaunchController.php
+++ app/Http/Controllers/LTI/LtiLaunchController.php
@@ -40,6 +40,6 @@ class LtiLaunchController
             'prompt'        => 'none',
         ];
 
-        return redirect(URI::of('https://sso.test.canvaslms.com/api/lti/authorize_redirect')->withQuery($parameters));
+        return redirect(url()->query('https://sso.test.canvaslms.com/api/lti/authorize_redirect', $parameters));
     }
 }
