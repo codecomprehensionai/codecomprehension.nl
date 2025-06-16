e0cf313195ebf844d69f188c83e4bd558429fbae
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-16 16:54:25 +0200

    fix column

--- database/migrations/0001_01_01_000004_create_courses_table.php
+++ database/migrations/0001_01_01_000004_create_courses_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->uuid('lti_id')->unique();
+            $table->string('name');
             $table->timestamps();
         });
     }
