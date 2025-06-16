9fda722b8deeab06bba546a201a9b5d6421b46da
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-16 14:28:29 +0200

    disable require json cause canvas sucks

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LTI\LtiLaunchController;
 use App\Http\Controllers\LTI\LtiCallbackController;
+use App\Http\Middleware\RequireJsonMiddleware;
 
 Route::prefix('v1')->as('v1:')->group(function (): void {
-    Route::post('oidc', LtiLaunchController::class)->name('oidc.launch');
-    Route::post('oidc/callback', LtiCallbackController::class)->name('oidc.callback');
+    Route::post('oidc', LtiLaunchController::class)->withoutMiddleware(RequireJsonMiddleware::class)->name('oidc.launch');
+    Route::post('oidc/callback', LtiCallbackController::class)->withoutMiddleware(RequireJsonMiddleware::class)->name('oidc.callback');
 });
