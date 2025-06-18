939c692957f2e04b8b768729aa71650b45f5ad4d
Mathieu <mathieu.janssen1@gmail.com>
2025-06-18 23:26:59 +0200

    Fix QuestionFactory to use correct enum for language selection
    (cherry picked from commit c3fa11aeae734cd785734b816025a354a04d7593)

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Enums\QuestionLevel;
 use App\Enums\QuestionType;
+use App\Enums\QuestionLanguage;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -20,7 +21,7 @@ class QuestionFactory extends Factory
     {
         return [
             /* Question metadata */
-            'language'                  => fake()->randomElement(QuestionType::cases()),
+            'language'                  => fake()->randomElement(QuestionLanguage::cases()),
             'type'                      => fake()->randomElement(QuestionType::cases()),
             'level'                     => fake()->randomElement(QuestionLevel::cases()),
             'estimated_answer_duration' => fake()->numberBetween(30, 300),
