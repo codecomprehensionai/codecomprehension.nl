0a6c103e89aedefd59c0c33fba1c0d935708a400
Mathieu <mathieu.janssen1@gmail.com>
2025-06-20 14:24:49 +0200

    start of create page

--- resources/views/livewire/assignment-teacher.blade.php
+++ resources/views/livewire/assignment-teacher.blade.php
@@ -5,6 +5,21 @@
     {{-- TODO: assignment details --}}
     <p>{{ $this->assignment->title }}</p>
 
+    <x-card class="py-13">
+        <div class="flex items-center justify-between">
+            <b class="text-2xl">{{ $this->assignment->title }}</b>
+            <div class="text-right">
+                <p>Due Date</p>
+                <p>{{ $this->assignment->deadline_at }}</p>
+            </div>
+        </div>
+    </x-card>
+    <div class="flex justify-center my-4">
+        <button
+            class="bg-white hover:bg-gray-50 text-gray-800 font-semibold py-2 px-4 border border-gray-200 rounded shadow">
+            + Add question
+        </button>
+    </div>
     {{-- TODO: questions block --}}
 
     {{-- TODO: foreach question --}}
