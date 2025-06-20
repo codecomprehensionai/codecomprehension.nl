5b87ab860b0b8c00c248c30970fdd5d474414d3b
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-20 09:56:43 +0200

    Show login error page

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController
+{
+    public function login()
+    {
+        // TODO: show nicer error
+        return 'Open the assignment in canvas.';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\JwksController;
 use App\Http\Controllers\LtiCallbackController;
 use App\Http\Controllers\LtiLaunchController;
@@ -8,7 +9,10 @@ use App\Http\Controllers\TeacherDashboardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('api/v1/oidc/jwks', JwksController::class)->name('oidc.jwks');
+Route::get('api/v1/jwks', JwksController::class)->name('oidc.jwks');
+Route::get('api/v1/oidc/jwks', JwksController::class); /* Legacy */
+
+Route::get('/', [DashboardController::class, 'login'])->name('login');
 
 Route::middleware(['guest'])->group(function () {
     Route::post('api/v1/oidc', LtiLaunchController::class)->name('oidc.launch');
@@ -16,7 +20,7 @@ Route::middleware(['guest'])->group(function () {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', fn () => Inertia::render('dashboard'))->name('dashboard');
+    Route::get('dashboard', fn() => Inertia::render('dashboard'))->name('dashboard');
 
     Route::get('teacher', [TeacherDashboardController::class, 'index'])->name('teacher.dashboard');
     Route::get('student', [StudentDashboardController::class, 'index'])->name('student.dashboard');
