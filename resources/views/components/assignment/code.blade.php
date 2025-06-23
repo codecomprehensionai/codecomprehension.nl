07c0926edc6a8c94bc23956ef401eff4947e97bd
Thijmen Batelaan <thijmen.batelaan@student.uva,nl>
2025-06-23 12:00:12 +0200

    wip

--- resources/views/components/assignment/code.blade.php
+++ resources/views/components/assignment/code.blade.php
@@ -1,5 +1,5 @@
-<div class="bg-blue-50 dark:bg-blue-900/50 p-4 rounded-lg mb-4">
-    <div class='flex gap-3 items-center justify-center rounded-lg'>
+<div class="bg-card text-card-foreground flex flex-col gap-6 rounded-xl border p-6 shadow-sm">
+    <div class='flex gap-3 items-center justify-start rounded-lg'>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
             class="lucide lucide-code">
@@ -8,8 +8,14 @@
         </svg>
         <span>Code to analyse</span>
     </div>
-    <div data-slot="card-content" className="px-6">
-        <div className="bg-gray-900 text-gray-100 p-4 rounded-lg font-mono text-sm overflow-x-auto">
+    <div
+    data-slot="card-description"
+    class="text-muted-foreground text-sm"
+    >
+    Study this code carefully before answering the questions
+    </div>
+    <div data-slot="card-content" class="px-6">
+        <div class="bg-gray-900 text-gray-100 p-4 rounded-lg font-mono text-sm overflow-x-auto">
             <pre><code>{{ $question->code }}</code></pre>
         </div>
     </div>
