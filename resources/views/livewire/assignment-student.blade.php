aab102383b8c1a93961ec0edbc08128de12e6ffa
Thijmen Batelaan <thijmen.batelaan@student.uva,nl>
2025-06-23 14:02:40 +0200

    x

--- resources/views/livewire/assignment-student.blade.php
+++ resources/views/livewire/assignment-student.blade.php
@@ -2,10 +2,11 @@
     <x-header />
     <div class="max-w-7xl mx-auto px-6 py-8">
         <x-assignment.title :$assignment />
-        @foreach ($this->assignment->questions as $question)
+        @php
+            $question = $assignment->questions[$index] ?? null;
+        @endphp
             <x-assignment.code :$question />
             <x-assignment.answer-box :$question />
-        @endforeach
     </div>
 </div>
 
