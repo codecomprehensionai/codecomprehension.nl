900051da02568bc658c3b89ef90d05fa353f7788
Thijmen Batelaan <thijmen.batelaan@student.uva,nl>
2025-06-23 13:28:26 +0200

    wip

--- resources/views/livewire/assignment-student.blade.php
+++ resources/views/livewire/assignment-student.blade.php
@@ -1,10 +1,11 @@
 <div>
     <x-header />
-    <x-assignment.title :$assignment />
-    @foreach ($this->assignment->questions as $question)
-        {{-- <x-assignment.question :$question /> --}}
-        <x-assignment.code :$question />
-        <x-assignment.answer-box :$question />
-    @endforeach
+    <div class="max-w-7xl mx-auto px-6 py-8">
+        <x-assignment.title :$assignment />
+        @foreach ($this->assignment->questions as $question)
+            <x-assignment.code :$question />
+            <x-assignment.answer-box :$question />
+        @endforeach
+    </div>
 </div>
 
