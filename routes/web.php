896701426b8deebe80bb255558cab2f97f28121a
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-24 14:20:03 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', Welcome::class)->name('login');
 
+Route::get('api/v1/oidc', [OidcController::class, 'launch'])->name('oidc.launch');
 Route::post('api/v1/oidc', [OidcController::class, 'launch'])->name('oidc.launch');
+
+Route::get('api/v1/oidc/callback', [OidcController::class, 'callback'])->name('oidc.callback');
 Route::post('api/v1/oidc/callback', [OidcController::class, 'callback'])->name('oidc.callback');
+
 Route::get('api/v1/jwks', JwksController::class)->name('oidc.jwks');
 Route::get('api/v1/oidc/jwks', JwksController::class); /* Legacy */
 
