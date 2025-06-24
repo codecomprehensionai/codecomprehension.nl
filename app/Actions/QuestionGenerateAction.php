6cf381501ef5153fffc3e3d41544a2d7fd80d876
Luca Castelnuovo <luca@castelnuovo.dev>
2025-06-24 10:36:34 +0200

    Remove JWTs

--- app/Actions/QuestionGenerateAction.php
+++ app/Actions/QuestionGenerateAction.php
@@ -19,8 +19,6 @@ final readonly class QuestionGenerateAction
         $aud = 'https://llm.codecomprehension.nl';
         $token = JwtKey::first()->sign($sub, $aud, now()->addDay());
 
-        $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjAxank2aGdlbWRzN2c5dzNhdzAzdzhmOXk2In0.eyJpc3MiOiJodHRwczovL2NvZGVjb21wcmVoZW5zaW9uLm5sIiwic3ViIjoiMTA0NDAwMDAwMDAwMDAwMzQwIiwiYXVkIjoiaHR0cHM6Ly9sbG0uY29kZWNvbXByZWhlbnNpb24ubmwiLCJleHAiOjE3NTA4Mzk3MzUsIm5iZiI6MTc1MDc1MzMzNSwiaWF0IjoxNzUwNzUzMzM1LCJqdGkiOiIwMWp5Z2dmcDYxZmt4Nm54MXF5bnNzN3IyciJ9.4Axr0pnx5HMN1l5m-NWPn_pQX7vkgMiwU-EmDhrYV93Bjeakz9y9xxAsuZg5-e9GAMNpL8q7uBKnM8QdwE-yXg';
-
         $response = Http::withToken($token)
             ->connectTimeout(3)
             ->timeout(120)
@@ -32,7 +30,7 @@ final readonly class QuestionGenerateAction
                     'description' => $assignment->description,
                 ],
                 'questions' => $assignment->questions
-                    ->map(fn (Question $question): array => QuestionData::from($question)->toArray())
+                    ->map(fn(Question $question): array => QuestionData::from($question)->toArray())
                     ->toArray(),
                 'new_question'        => $newQuestionData->toArray(),
                 'new_question_prompt' => $newQuestionPrompt,
--- app/Actions/QuestionGradeAction.php
+++ app/Actions/QuestionGradeAction.php
@@ -20,8 +20,6 @@ final readonly class QuestionGradeAction
         $aud = 'https://llm.codecomprehension.nl';
         $token = JwtKey::first()->sign($sub, $aud, now()->addDay());
 
-        $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjAxank2aGdlbWRzN2c5dzNhdzAzdzhmOXk2In0.eyJpc3MiOiJodHRwczovL2NvZGVjb21wcmVoZW5zaW9uLm5sIiwic3ViIjoiMTA0NDAwMDAwMDAwMDAwMzQwIiwiYXVkIjoiaHR0cHM6Ly9sbG0uY29kZWNvbXByZWhlbnNpb24ubmwiLCJleHAiOjE3NTA4Mzk3MzUsIm5iZiI6MTc1MDc1MzMzNSwiaWF0IjoxNzUwNzUzMzM1LCJqdGkiOiIwMWp5Z2dmcDYxZmt4Nm54MXF5bnNzN3IyciJ9.4Axr0pnx5HMN1l5m-NWPn_pQX7vkgMiwU-EmDhrYV93Bjeakz9y9xxAsuZg5-e9GAMNpL8q7uBKnM8QdwE-yXg';
-
         $response = Http::withToken($token)
             ->connectTimeout(3)
             ->timeout(120)
@@ -33,7 +31,7 @@ final readonly class QuestionGradeAction
                     'description' => $assignment->description,
                 ],
                 'questions' => $assignment->questions
-                    ->map(fn (Question $question): array => QuestionData::from($question)->toArray())
+                    ->map(fn(Question $question): array => QuestionData::from($question)->toArray())
                     ->toArray(),
                 'existing_question'      => $existingQuestionData->toArray(),
                 'update_question'        => $updateQuestionData->toArray(),
