163c54a32998a4f5c1b1f4208c9014c3b9545756
Steincookies <stein.pleiter@gmail.com>
2025-06-26 14:30:30 +0200

    update job

--- app/Jobs/SubmitJob.php
+++ app/Jobs/SubmitJob.php
@@ -2,20 +2,26 @@
 
 namespace App\Jobs;
 
+use App\Enums\AssignmentStatus;
 use App\Models\Assignment;
 use App\Models\User;
+use Bus;
+use Illuminate\Bus\Batch;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 
+/* This job makes sure that every submission get's graded. Afterwards it sets
+ * the database entry for this assignment and user to GRADED. During this 
+ * process the assignment status is SUBMITTED.
+ */
 class SubmitJob implements ShouldQueue
 {
-    // TODO: Make this
     use Queueable;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(User $user, Assignment $assignment, array $submissions)
+    public function __construct(protected User $user, protected Assignment $assignment, protected array $submissions)
     {
         //
     }
@@ -25,5 +31,29 @@ class SubmitJob implements ShouldQueue
      */
     public function handle(): void
     {
+        // Set the assignmentstatus of this user for this assignment to submitted
+        $this->assignment->assignmentStatuses()->updateOrCreate(
+            ['user_id' => $this->user->id],
+            ['status' => AssignmentStatus::SUBMITTED]
+        );
+
+        // Create jobs for grading the submissions
+        $jobs = [];
+        foreach ($this->submissions as $submission) {
+            $jobs[] = new CalculateSubmissionScoreJob($submission);
+        }
+
+        // Batch the jobs and afterwards set assignment status to GRADED
+        Bus::batch($jobs)
+            ->then(function (Batch $batch) {
+                // Set AssignmentStatus to graded in DB
+                $this->assignment->assignmentStatuses()->updateOrCreate(
+                    ['user_id' => $this->user->id],
+                    ['status' => AssignmentStatus::GRADED]
+                );
+            })
+            ->name('Grade all submission')
+            ->dispatch();
+
     }
 }
