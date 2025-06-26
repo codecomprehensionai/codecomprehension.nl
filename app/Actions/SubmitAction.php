1e42d49ddbb0ef4e33f064559d198d6d687a1224
Mathieu <mathieu.janssen1@gmail.com>
2025-06-26 13:40:34 +0200

    Add SubmitAction class to handle assignment submissions and grading

---
+++ app/Actions/SubmitAction.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions;
+
+use App\Enums\AssignmentStatus;
+use App\Jobs\CalculateSubmissionScoreJob;
+use App\Models\Assignment;
+use App\Models\Submission;
+use App\Models\User;
+use Illuminate\Bus\Batch;
+use Illuminate\Support\Facades\Bus;
+
+final readonly class SubmitAction
+{
+    // TODO: Link to QuestionGradeAction
+    public function handle(User $user, Assignment $assignment, array $submissions)
+    {
+        // Set the assignmentstatus of this user for this assignment to submitted
+        $assignment->assignmentStatuses()->updateOrCreate(
+            ['user_id' => $user->id],
+            ['status' => AssignmentStatus::SUBMITTED]
+        );
+
+        // Create jobs for grading the submissions
+        $jobs = [];
+        foreach ($submissions as $submission) {
+            $jobs[] = new CalculateSubmissionScoreJob($submission);
+        }
+
+        // Batch the jobs and afterwards set assignment status to GRADED
+        Bus::batch($jobs)
+            ->then(function (Batch $batch) use ($user, $assignment) {
+
+                // When job comes back set status to graded
+                $assignment->assignmentStatuses()->updateOrCreate(
+                    ['user_id' => $user->id],
+                    ['status' => AssignmentStatus::GRADED]
+                );
+            })
+            ->name('Grade all submission')
+            ->dispatch();
+    }
+}
