2fc348dc7889d27390c2a66280f4b142769e01e1
Mathieu <mathieu.janssen1@gmail.com>
2025-06-26 13:20:11 +0200

    wip

--- app/Actions/SubmitAndGradeAction.php
+++ app/Actions/SubmitAndGradeAction.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace App\Actions;
 
 use App\Enums\AssignmentStatus;
+use App\Jobs\CalculateSubmissionScoreJob;
 use App\Models\Assignment;
+use App\Models\Submission;
 use App\Models\User;
-use LibDNS\Records\Question;
 
 final readonly class SubmitAndGradeAction
 {
     // TODO: Link to QuestionGradeAction
-    public function handle(User $user, Assignment $assignment, array $questions)
+    public function handle(User $user, Assignment $assignment, array $submissions)
     {
         // Set the assignmentstatus of this user for this assignment to submitted
         $assignment->assignmentStatuses()->updateOrCreate(
@@ -20,9 +21,26 @@ final readonly class SubmitAndGradeAction
             ['status' => AssignmentStatus::SUBMITTED]
         );
 
-        // Launch job to grade all questions
+        // Launch job that first grades all questions and wait for it 
+        $jobs = [];
+        foreach ($submissions as $submission) {
+            $job = new CalculateSubmissionScoreJob($submission);
+            dispatch($job);
+            $jobs[] = $job;
+        }
+
+
+        foreach ($jobs as $job) {
+            $job->handle();
+        }
+
 
 
         // When job comes back set status to graded
+        // TODO: Add logic for this code that waits for everything to be graded...
+        $assignment->assignmentStatuses()->updateOrCreate(
+            ['user_id' => $user->id],
+            ['status' => AssignmentStatus::GRADED]
+        );
     }
 }
---
+++ app/Jobs/SubmitAndGradeJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Assignment;
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class SubmitAndGradeJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(User $user, Assignment $assignment, array $submissions)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+    }
+}
