e14123a1944d27eb0eee0c0a8631fec55e6449d9
Mathieu <mathieu.janssen1@gmail.com>
2025-06-26 10:42:40 +0200

    UserAssignmentStatus Model

---
+++ app/Enums/AssignmentStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum AssignmentStatus: string
+{
+
+    case UNSTARTED = "unstarted";
+    case SUBMITTED = "submitted";
+    case IN_PROGRESS = "in_progress";
+    case GRADED = "graded";
+}
---
+++ app/Models/UserAssignmentStatus.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\AssignmentStatus;
+use App\Models\Assignment;
+use App\Models\Submission;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserAssignmentStatus extends Model
+{
+    /*
+     * To check if a user has submitted a whole assignment, we keep track of
+     * this using the AssignmentStatus enum. For every user and assignment combo
+     * the AssignmentStatus is kept track of. 
+     */
+
+    /**
+     * The user that the AssignmentStatus belongs to.
+     *
+     * @return BelongsTo<User, UserAssignmentStatus>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The assignment that AssignmentStatus belongs to.
+     *
+     * @return BelongsTo<Assignment, UserAssignmentStatus>
+     */
+    public function assignment(): BelongsTo
+    {
+        return $this->belongsTo(Assignment::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'assignment_status' => AssignmentStatus::class,
+        ];
+    }
+
+}
